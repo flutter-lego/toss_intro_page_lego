b46ea95fe93a5fc1f1689c79e28c79955cb41a59
june lee <melodysdreamj@gmail.com>
2024-07-17 05:53:25 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/toss_intro_page_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForTossIntroPageLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/run_app/toss_intro_page_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverRunApp()
+// Widget coverTossIntroPageLego(Widget widget) {
+//   return widget;
+// }
